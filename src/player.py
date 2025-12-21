4869383e59aeb8849c608f66359b01fcb519e389
Justin-Wt <halimjustin249@gmail.com>
2025-12-21 13:03:56 +0800

    Enhance Player class with race and rank features
    Added race attribute and rank calculation to Player class.

--- src/player.py
+++ src/player.py
@@ -1,7 +1,21 @@
 from inventory import Inventory
+LIGHT_GREEN = "\033[1;32m"
+RED = "\033[0;31m"
+BLUE = "\033[0;34m"
+YELLOW = "\033[1;33m"
+GREEN = "\033[0;32m"
+END = "\033[0m"
+race_color={
+    "Elf":LIGHT_GREEN,
+    "Orc":GREEN,
+    "Demon":RED,
+    "Human":YELLOW
+}
+rank=[("F",20),("E-",10),("E",10),("E+",10),("D-",20),("D",20),("D+",20),("C-",30),("C",30),("C+",30),("B-",40),("B",40),("B+",40),("A-",50),("A",50),("A+",50),("S",70)]
 class Player:
-	def __init__(self, name, hp=0, strength=0, defense=0, level=1, weapon="Empty", health_potion=0, mana_potion=0,gold=200 ,armor="Empty"):
+	def __init__(self, name,race="none", hp=0, strength=0, defense=0, level=1, weapon="Empty", health_potion=0, mana_potion=0,gold=200 ,armor="Empty"):
 		self.name = name
+		self.race = race
 		self.hp = max(0,hp)
 		self.strength = max(0,strength)
 		self.defense = max(0,defense)
@@ -11,6 +25,12 @@ class Player:
 		self.health_potion=max(0,health_potion)
 		self.mana_potion=max(0,mana_potion)
 		self.armor=armor
+		requirements=0
+		for ranks,amount in rank:
+			total = self.hp + self.strength + self.defense
+			requirements+=amounta
+			if total>=requirements:
+				self.rank=ranks
         # If no inventory is given, start with an empty list
 		self.inventory =Inventory(self.weapon,self.health_potion,self.mana_potion,self.gold,self.armor)
 	def take_damage(self,enemy):
@@ -46,5 +66,5 @@ class Player:
 	def show_inventory(self):
 		return f"{'':-<40}Inventory:\n{self.inventory}\n{'':—<40}"
 	def __str__(self):
-		total = self.hp + self.strength + self.defense
-		return f"{self.name:<10}{'level'}:{self.level}\n{'':—<20} \n HP: {self.hp}\n STR: {self.strength}\n DEF: {self.defense}\n{'':—<20}"
+		color=race_color.get(self.race,"")
+		return f"{self.name:<10}{'level'}:{self.level}\n{'':—<20} \nRank:{self.rank}\nRace: {color}{self.race}{END}\nHP: {RED}{self.hp}{END}\nSTR: {YELLOW}{self.strength}{END}\nDEF: {BLUE}{self.defense}{END}\n{'':—<20}"
