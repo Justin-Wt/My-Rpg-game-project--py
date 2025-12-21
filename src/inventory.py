669b2822eea36d876b60342749057ca8e15e589a
Justin-Wt <halimjustin249@gmail.com>
2025-12-21 13:02:21 +0800

    Enhance Inventory class with gold and new items
    Updated inventory management to include gold tracking and new stackable items.

--- src/inventory.py
+++ src/inventory.py
@@ -2,23 +2,23 @@ class Inventory:
     def __init__(self, weapon="Empty", health_potion=0, mana_potion=0,gold=200, armor="Empty"):
         self.weapon = weapon
         self.armor = armor
+        self.gold=gold
         self.items = {
-            "gold": gold,
             "health potion": health_potion,
             "mana potion": mana_potion
         }
         self.accesory=[]
 
     def add_item(self, item, amount=1):
-        stackable=["health potion","mana potion","gold"]
+        stackable=["health potion","mana potion","gold","slime gel","sticky core","fang"]
         weapon=["sword","dagger"]
         # Non-stackable items
         if item in stackable:
             # Stackable items
             if item in self.items:
-                self.items[item] += amount
+                self.items[item] += int(amount)
             else:
-                self.items[item] = amount
+                self.items[item] = int(amount)
             print(f"{amount} {item}(s) added to inventory")
         else:
             if item in weapon:
@@ -30,7 +30,10 @@ class Inventory:
             print(f"{item} equipped!")
 
     def remove_item(self, item, amount=1):
-        if item in self.items and self.items[item]>=amount:
+        if item == "gold" and self.gold>=amount:
+            self.gold-=amount
+            return True
+        elif item in self.items and self.items[item]>=amount:
             self.items[item] -= amount
             if self.items[item] == 0:
                 del self.items[item]
@@ -44,6 +47,7 @@ class Inventory:
 
     def __str__(self):
         lines = []
+        lines.append(f"        gold:\n            -{self.gold}")
         lines.append(f"        weapon:\n           -{self.weapon}")
         lines.append(f"        armor:\n           -{self.armor}")
         if self.accesory:
