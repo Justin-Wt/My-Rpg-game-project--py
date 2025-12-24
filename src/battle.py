29dd50e5d9d44e125048006658d0a3b01d5cd3dc
Justin-Wt <halimjustin249@gmail.com>
2025-12-24 23:37:41 +0800

    Refactor battle logic and add attack function

--- src/battle.py
+++ src/battle.py
@@ -1,22 +1,39 @@
 from player import Player
-from enemy import Enemy,enemy_picker
+from enemy import Enemy
 from inventory import Inventory
 RED = "\033[0;31m"
 BLUE = "\033[0;34m"
 YELLOW = "\033[1;33m"
 END = "\033[0m"
-def status(player,enemy):
-	print(f"{'':-<55}\n|lv.{enemy.level:<2} {enemy.name:<20}|lv.{player.level:<2} {player.name:<20}|\n{'':-<55}\n|{RED}Hp:{enemy.hp:<23}{END}|{RED}Hp:{player.hp:<23}{END}|\n|{YELLOW}strength:{enemy.strength:<17}{END}|{YELLOW}strength:{player.strength:<17}{END}|\n|{BLUE}defense:{enemy.defense:<18}{END}|{BLUE}defense:{player.defense:<18}{END}|\n{'':-<55}")
+def attack(attacker,enemy):
+		damage=max(0,attacker.strength-enemy.defense)
+		enemy.hp=max(0,enemy.hp-damage)
+		print(f"you've dealth {damage} damage")
+		enemy_damage=max(0,enemy.strength-attacker.defense)
+		attacker.hp=max(0,attacker.hp-enemy_damage)
+		print(f"The enemy has counter with {enemy_damage} damage!")
+		if attacker.hp==0:
+		    print("you've lost")
+		elif enemy.hp==0:
+			loot=enemy.drop_loot()
+			if loot=="nothing":
+				None
+			else:
+				for item,amount in loot:
+					attacker.inventory.add_item(item,amount)
+			return damage
+	
+def status(attacker,enemy):
+	print(f"{'':-<55}\n|lv.{enemy.level:<2} {enemy.name:<20}|lv.{attacker.level:<2} {attacker.name:<20}|\n{'':-<55}\n|{RED}Hp:{enemy.hp:<23}{END}|{RED}Hp:{attacker.hp:<23}{END}|\n|{YELLOW}strength:{enemy.strength:<17}{END}|{YELLOW}strength:{attacker.strength:<17}{END}|\n|{BLUE}defense:{enemy.defense:<18}{END}|{BLUE}defense:{attacker.defense:<18}{END}|\n{'':-<55}")
 
 #battle test
 if __name__=="__main__":
-	player=Player("justin",hp=10,strength=2222222,defense=4,level=3,weapon="sword",armor="iron armor")
-	enemy=enemy_picker(1)
-	status(player,enemy)
-	enemy.attack(player)
-	player.take_damage(enemy)
-	status(player,enemy)
-	player.attack(enemy)
-	status(player,enemy)
+	player=Player("justin","Elf",hp=1011,strength=2222222,defense=4,weapon="sword",armor="iron armor")
+	enemy1=Enemy.from_area("forest","normal")
+	status(player,enemy1)
+	attack(player,enemy1)
+	status(player,enemy1)
+	attack(player,enemy1)
+	status(player,enemy1)
 	print(player.inventory)
 	input()
