287304644fa6800d701446f0fe02c0628ed2721e
Justin-Wt <halimjustin249@gmail.com>
2026-01-07 23:40:55 +0800

    Enhance status management methods in Entity
    Refactor status handling in Entity class

--- src/entity.py
+++ src/entity.py
@@ -1,21 +1,21 @@
-from status import  StatusEffect
 class Entity:
-    def __init__(self,name,hp,defense,strength):
-        self.name=name
-        self.hp=hp
-        self.defense=defense
-        self.strength=strength
-        self.statuses=[]
-    def apply_status(self,status):
-        self.statuses.append(status)
+    def __init__(self, name, hp, defense, strength):
+        self.name = name
+        self.hp = hp
+        self.defense = defense
+        self.strength = strength
+        self.statuses = []
+    def has_status(self, name):
+        return any(status.name == name for status in self.statuses)
+    def can_act(self):
+        return not self.has_status("stun")
+    def apply_status(self, status):
         status.on_apply(self)
-    def status_attack(self):
+        self.statuses.append(status)
+
+    def update_statuses(self):
         for status in self.statuses[:]:
-            status.on_start(self)
-            status.on_end(self)
-            if status.duration<=0:
+            status.on_tick(self)
+            if status.is_expired():
+                status.on_end(self)
                 self.statuses.remove(status)
-                print(f"the {status} on {self.name} has worn off")
-    def is_immune(self,status):
-        return False
-    
